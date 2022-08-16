73d2f5f93081465870b9405cf37420e8c5118d53
Gabriel Zanata <zanatag.gz@gmail.com>
2022-08-16 13:01:04 -0300

    alterando campo de visão

--- src/client/map.cpp
+++ src/client/map.cpp
@@ -655,12 +655,12 @@ void Map::setAwareRange(const AwareRange& range)
 
 void Map::resetAwareRange()
 {
-    AwareRange range;
-    range.left = 8;
-    range.top = 6;
-    range.bottom = 7;
-    range.right = 9;
-    setAwareRange(range);
+   AwareRange range;
+   range.left = 15; //Change this to = maxClientViewportX
+   range.top = 15; //Change this to = maxClientViewportY
+   range.bottom = range.top+1;
+   range.right = range.left+1;
+   setAwareRange(range);
 }
 
 int Map::getFirstAwareFloor()
